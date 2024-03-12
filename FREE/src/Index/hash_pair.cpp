7dfb89a50036d46cc2d59bf7167e58fb8ef5e424
LING ZHANG <junelena1313@gmail.com>
2024-03-11 19:52:27 -0500

    std::size_t to size_t

--- FREE/src/Index/hash_pair.cpp
+++ FREE/src/Index/hash_pair.cpp
@@ -3,30 +3,30 @@
 struct hash_pair hash_pair;
 
 template <typename T>
-void hash_combine (std::size_t& seed, const T& val) {
+void hash_combine (size_t& seed, const T& val) {
     seed ^= std::hash<T>()(val) + 0x9e3779b9 + (seed<<6) + (seed>>2);
 }
 
 // auxiliary generic functions to create a hash value using a seed
 template <typename T, typename... Types>
-void hash_combine (std::size_t& seed, const T& val, const Types&... args) {
+void hash_combine (size_t& seed, const T& val, const Types&... args) {
     hash_combine(seed,val);
     hash_combine(seed,args...);
 }
 
 // optional auxiliary generic functions to support hash_val() without arguments
-void hash_combine (std::size_t& seed) {}
+void hash_combine (size_t& seed) {}
 
 //  generic function to create a hash value out of a heterogeneous list of arguments
 template <typename... Types>
-std::size_t hash_val (const Types&... args) {
-    std::size_t seed = 0;
+size_t hash_val (const Types&... args) {
+    size_t seed = 0;
     hash_combine(seed, args...);
     return seed;
 }
 
 template <class T1, class T2>
-std::size_t hash_pair::operator()(const std::pair<T1, T2>& p) const
+size_t hash_pair::operator()(const std::pair<T1, T2>& p) const
 {
     return hash_val(p.first, p.second);
 }
--- FREE/src/Index/hash_pair.hpp
+++ FREE/src/Index/hash_pair.hpp
@@ -1,6 +1,8 @@
 #ifndef FREE_INDEX_HASH_PAIR_HPP_
 #define FREE_INDEX_HASH_PAIR_HPP_
 
+#include <cstddef>
+
 // ------------------------------------------------------------------------------------------------------
 //  Tuple Hash Functions
 //  Hashing strategy of tuples: https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3876.pdf
@@ -8,7 +10,7 @@
 
 struct hash_pair {
     template <class T1, class T2>
-    std::size_t operator()(const std::pair<T1, T2>& p) const;
-};};
+    size_t operator()(const std::pair<T1, T2>& p) const;
+};
 
 #endif // FREE_INDEX_HASH_PAIR_HPP_
