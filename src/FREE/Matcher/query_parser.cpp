9190027247d658fc93f29723a8d2b5f4a1cbff6e
LING ZHANG <junelena1313@gmail.com>
2024-03-18 16:38:57 -0500

    Update query_parser.cpp

--- src/FREE/Matcher/query_parser.cpp
+++ src/FREE/Matcher/query_parser.cpp
@@ -1,4 +1,5 @@
 #include "query_parser.hpp"
+#include "../../utils/reg_utils.hpp"
 
 #include <algorithm>
 #include <iostream>
@@ -10,52 +11,11 @@ using OrNode = free_index::OrNode;
 using NullNode = free_index::NullNode;
 using LiteralNode = free_index::LiteralNode;
 
-//The true special/meta chars: '{', '}', '['. ']', '(', ')', '^', '$', '.', '*', '+', '?', '|'
-const std::unordered_set<char> k_special_chars{'{', '[', '^', '$', '.', '*', '+', '?', '|'};
-const std::unordered_set<char> k_special_classes{'w', 'W', 'a', 'b', 'B', 'd', 'D', 'l', 'p',
-                                                 's', 'S', 'u', 'x'};
-
 enum OP { kAnd, kOr };
 
 // forward declaration
 std::unique_ptr<QueryPlanNode> build_rooted_op_plan(const std::string & reg_str, OP op);
 std::unique_ptr<QueryPlanNode> build_rooted_plan(const std::string & reg_str);
-
-bool char_escaped(const std::string &line, std::size_t pos) {
-    auto temp_pos = pos;
-    int num_escaped = 0;
-    // count number of escape characters before the char
-    while (temp_pos > 0) {
-        if (line.at(--temp_pos) != '\\') 
-            break;
-        num_escaped++;
-    }
-    return num_escaped % 2 == 1;
-}
-
-std::string remove_escape(const std::string & line) {
-    // remove escape (trick method. should only remove single \ and turning \\ to \)
-    std::string result(line);
-    result.erase(std::remove(result.begin(), result.end(), '\\'), result.end());
-    return result;
-}
-
-bool check_special(const std::string & reg_str) {
-    // check if the string contains 
-    //    1. special chars not escaped; or
-    //    2. special class chars that are espcaed
-
-    if (reg_str.empty()) return true;
-    for (size_t i = 0; i < reg_str.size(); i++) {
-        if ((k_special_chars.find(reg_str.at(i)) != k_special_chars.end() &&
-             !char_escaped(reg_str, i)) || 
-            (k_special_classes.find(reg_str.at(i)) != k_special_classes.end() &&
-             char_escaped(reg_str, i))) {
-            return true;
-        }
-    }
-    return false;
-}
 
 std::unique_ptr<QueryPlanNode> make_const_node(const std::string & token) {
     if (token.empty()) {
