e8065d88277231bb9232e9bef1416204c527c44e
LING ZHANG <junelena1313@gmail.com>
2024-05-12 17:13:41 -0500

    Update benchmark.cpp

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -30,7 +30,11 @@ int main(int argc, char** argv) {
 
     std::vector<std::string> regexes;
     std::vector<std::string> lines;
+#ifdef NDEBUG
     status = readWorkload(expr_info, regexes, lines, 100000);
+#else
+    status = readWorkload(expr_info, regexes, lines);
+#endif
     if (status == EXIT_FAILURE) {
         return EXIT_FAILURE;
     }
