f087b6e846f32a2c1e506f5c9b4ac20cca7539dd
LING ZHANG <junelena1313@gmail.com>
2024-05-12 17:46:14 -0500

    echo cmd

--- run_expr_traffic.sh
+++ run_expr_traffic.sh
@@ -6,8 +6,10 @@ mkdir -p ${dirname}
 # Free
 for n in 2 4 6 8 10; do
     for c in 0.01 0.02 0.03 0.05 0.07 0.1 0.2 0.3 0.5 0.7; do
+        echo benchmark.out FREE -t 1 -w 1 -o ${dirname} -n ${n} --presuf -c ${c}
         ./benchmark.out FREE -t 1 -w 1 -o ${dirname} -n ${n} --presuf -c ${c}
         for t in 1 2 4 6 8 10; do
+            echo benchmark.out FREE -t ${t} -w 1 -o ${dirname} -n ${n} -c ${c} 
             ./benchmark.out FREE -t ${t} -w 1 -o ${dirname} -n ${n} -c ${c}
         done
     done
@@ -15,6 +17,7 @@ done
 # Best
 for c in 0.01 0.02 0.03 0.05 0.07 0.1 0.12 0.15 0.2 0.3 0.5; do
     for t in 1 2 4; do
+        echo benchmark.out BEST -t ${t} -w 1 -o ${dirname} -c ${c}
         ./benchmark.out BEST -t ${t} -w 1 -o ${dirname} -c ${c}
     done
 done
