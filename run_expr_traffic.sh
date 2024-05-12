76d775d5ef03d04481ff4ee3477aa3e3c0dd5849
LING ZHANG <junelena1313@gmail.com>
2024-05-12 16:13:39 -0500

    update script
    no multi for pre-suf

--- run_expr_traffic.sh
+++ run_expr_traffic.sh
@@ -6,9 +6,9 @@ mkdir -p ${dirname}
 # Free
 for n in 2 4 6 8 10; do
     for c in 0.01 0.02 0.03 0.05 0.07 0.1 0.2 0.3 0.5 0.7; do
+        ./benchmark.out FREE -t ${t} -w 1 -o ${dirname} -n ${n} --presuf -c ${c}
         for t in 1 2 4 6 8 10; do
             ./benchmark.out FREE -t ${t} -w 1 -o ${dirname} -n ${n} -c ${c}
-            ./benchmark.out FREE -t ${t} -w 1 -o ${dirname} -n ${n} --presuf -c ${c}
         done
     done
 done
