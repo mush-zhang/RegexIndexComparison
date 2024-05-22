cc6f31d0fd7349a31025b7351b28b914e44207f2
LING ZHANG <junelena1313@gmail.com>
2024-05-22 16:14:00 -0500

    Update run_expr.sh

--- run_expr.sh
+++ run_expr.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 
 # get default scalefactor
-if [ $1 == "traffic" ]; then
+if [ "$1" == "traffic" ]; then
     wl_num=1
-elif [ $1 == "db_x" ]; then
+elif [ "$1" == "db_x" ]; then
     wl_num=2
-elif [ $1 == "sys_y" ]; then
+elif [ "$1" == "sys_y" ]; then
     wl_num=3
 else
     exit 1
 fi
+
+timeout_prefix="timeout 6h --foreground"
+
 dirname=result/${1}_result_new
 echo ${dirname}
 mkdir -p ${dirname} 
@@ -21,21 +24,21 @@ num_repeat=5
 for c in ${sel_list[*]}; do
     for t in ${thread_list[*]}; do
         echo benchmark.out BEST -t ${t} -w ${wl_num} -o ${dirname} -c ${c} -e ${num_repeat}
-        timeout 6h ./benchmark.out BEST -t ${t} -w ${wl_num} -o ${dirname} -c ${c} -e ${num_repeat}
+        ${timeout_prefix} ./benchmark.out BEST -t ${t} -w ${wl_num} -o ${dirname} -c ${c} -e ${num_repeat}
     done
 done
 # Free
 for n in 2 4 6 8 10 12 14 16; do
     for c in ${sel_list[*]}; do
         echo benchmark.out FREE -t 1 -w ${wl_num} -o ${dirname} -n ${n} --presuf -c ${c} -e ${num_repeat}
-        timeout 6h ./benchmark.out FREE -t 1 -w ${wl_num} -o ${dirname} -n ${n} --presuf -c ${c} -e ${num_repeat}
+        ${timeout_prefix} ./benchmark.out FREE -t 1 -w ${wl_num} -o ${dirname} -n ${n} --presuf -c ${c} -e ${num_repeat}
         for t in ${thread_list[*]}; do
             echo benchmark.out FREE -t ${t} -w ${wl_num} -o ${dirname} -n ${n} -c ${c} -e ${num_repeat}
-            timeout 6h ./benchmark.out FREE -t ${t} -w ${wl_num} -o ${dirname} -n ${n} -c ${c} -e ${num_repeat}
+            ${timeout_prefix} ./benchmark.out FREE -t ${t} -w ${wl_num} -o ${dirname} -n ${n} -c ${c} -e ${num_repeat}
         done
     done
 done
 echo benchmark.out FAST -t 1 -w ${wl_num} -o ${dirname}  --relax DETERM -e ${num_repeat}
-timeout 6h ./benchmark.out FAST -t 1 -w ${wl_num} -o ${dirname} --relax DETERM -e ${num_repeat}
+${timeout_prefix} ./benchmark.out FAST -t 1 -w ${wl_num} -o ${dirname} --relax DETERM -e ${num_repeat}
 echo benchmark.out FAST -t 1 -w ${wl_num} -o ${dirname}  --relax RANDOM -e ${num_repeat}
-timeout 6h ./benchmark.out FAST -t 1 -w ${wl_num} -o ${dirname} --relax RANDOM -e ${num_repeat}
+${timeout_prefix} ./benchmark.out FAST -t 1 -w ${wl_num} -o ${dirname} --relax RANDOM -e ${num_repeat}
