b6120d29fb10e5ee346c4cffdfb99e202d134f15
LING ZHANG <junelena1313@gmail.com>
2024-06-07 15:13:53 -0500

    Update download_warc.sh

--- data/webpages/download_warc.sh
+++ data/webpages/download_warc.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
-PATH_FILE="warc.paths.gz"
+PATH_FILE="warc.paths"
 
 FILE_LIMIT=1
 
-wget https://data.commoncrawl.org/crawl-data/CC-MAIN-2013-48/${PATH_FILE}
-gzip -d ${PATH_FILE}
+wget https://data.commoncrawl.org/crawl-data/CC-MAIN-2013-48/${PATH_FILE}.gz
+gzip -d ${PATH_FILE}.gz
 
 num_file_read=0
 while read -r line; do
     wget https://data.commoncrawl.org/${line}
-    
+
     num_file_read=$((num_file_read+1))
 
     if [ ${num_file_read} -gt FILE_LIMIT ]; then
