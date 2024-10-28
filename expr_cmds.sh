5e724462a79966f794ce36c27bca386cf623bc33
LING ZHANG <junelena1313@gmail.com>
2024-10-28 17:13:01 -0500

    Update expr_cmds.sh

--- expr_cmds.sh
+++ expr_cmds.sh
@@ -1,10 +1,30 @@
 #! /bin/bash
 
+# Expr 2
 for qcount in 100 500 2000 2500 5000; do
-    ./run_expr.sh -w synthetic_q${qcount}_d20000 \
+    ./run_expr.sh -w synthetic_expr2_q${qcount}_d20000 \
     -r data/synthetic/expr2/queries/query_workload_${qcount}.txt \
     -d data/synthetic/expr2/datasets/dataset_20000.txt
+done
 for dcount in 20000 40000 60000 80000 100000; do
-    ./run_expr.sh -w synthetic_q1000_d${dcount} \
+    ./run_expr.sh -w synthetic_expr2_q1000_d${dcount} \
     -r data/synthetic/expr2/queries/query_workload_1000_for_${dcount}.txt \
     -d data/synthetic/expr2/datasets/dataset_${dcount}.txt
+done
+
+# Expr 4
+for rob_wl in 1 2 3 4; do
+    for perc in 10 30 50; do
+        ./run_expr.sh -w synthetic_expr4_rob0${rob_wl}_${perc} \
+        -r data/synthetic/expr4/queries/Rob0${rob_wl}_queries_${perc}pct.txt \
+        -d data/synthetic/expr4/datasets/Rob0${rob_wl}.txt
+    done
+done
+
+# Expr 1
+for expr in 0 1 2 3; do
+    std_val=$(( ($expr + 1) * 100 ))
+    ./run_expr.sh -w synthetic_expr1_${expr} \
+    -r data/synthetic/expr1/queries/query_workload_1000_for_${dcount}.txt \
+    -d data/synthetic/expr1/data_${expr}_std${std_val}.txt
+done
