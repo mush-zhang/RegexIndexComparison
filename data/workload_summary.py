88ac98a173750e57009a70611cebe5b4ab178541
LING ZHANG <junelena1313@gmail.com>
2024-11-06 13:20:16 -0600

    Update workload_summary.py

--- data/workload_summary.py
+++ data/workload_summary.py
@@ -13,151 +13,81 @@ import os
 DATA_ROOT = '.'
 
 
-# # ## Webpages
+# ## Webpages
 
-# # In[ ]:
-
-
-# dataset_dir = os.path.join(DATA_ROOT, 'webpages/processed')
+# In[ ]:
 
 
-# # In[ ]:
+dataset_dir = os.path.join(DATA_ROOT, 'webpages/processed')
 
 
-# char_set = set()
-# line_count = 0
-# ave_line_len = 0
-# for fn in os.listdir(dataset_dir):
-#     with open(os.path.join(dataset_dir, fn), 'r', encoding='latin') as file:
-#         curr_line = file.read()
-#         ave_line_len = (line_count * ave_line_len + len(curr_line)) / (line_count + 1)
-#         char_set = char_set.union(set(curr_line))
-#         line_count += 1
+# In[ ]:
 
 
-# # In[ ]:
+char_set = set()
+line_count = 0
+ave_line_len = 0
+for fn in os.listdir(dataset_dir):
+    with open(os.path.join(dataset_dir, fn), 'r', encoding='latin') as file:
+        curr_line = file.read()
+        if len(line) < 3:
+            continue
+        ave_line_len = (line_count * ave_line_len + len(curr_line)) / (line_count + 1)
+        char_set = char_set.union(set(curr_line))
+        line_count += 1
 
 
-# print('Webpages')
-# print(f'\t\tAlphabet Size: {len(char_set)}')
-# print(f'\t\tDataset Size: {line_count}')
-# print(f'\t\tAverage Num Chars in Dataset: {ave_line_len}')
+# In[ ]:
 
 
-# # ## DBLP
-
-# # In[ ]:
-
-
-# dataset_fn = os.path.join(DATA_ROOT, 'dblp/small/authors.txt')
+print('Webpages')
+print(f'\t\tAlphabet Size: {len(char_set)}')
+print(f'\t\tDataset Size: {line_count}')
+print(f'\t\tAverage Num Chars in Dataset: {ave_line_len}')
 
 
-# # In[ ]:
+# ## DBLP
+
+# In[ ]:
 
 
-# with open(dataset_fn, 'r') as file:
-#     dataset = file.readlines()
-
-# char_set = set()
-# line_count = 0
-# ave_line_len = 0
-# for line in dataset:
-#     line = line.rstrip()
-#     ave_line_len = (line_count * ave_line_len + len(line)) / (line_count + 1)
-#     char_set = char_set.union(set(line))
-#     line_count += 1
+dataset_fn = os.path.join(DATA_ROOT, 'dblp/small/authors.txt')
 
 
-# # In[ ]:
+# In[ ]:
 
 
-# print('DBLP')
-# print(f'\t\tAlphabet Size: {len(char_set)}')
-# print(f'\t\tDataset Size: {line_count}')
-# print(f'\t\tAverage Num Chars in Dataset: {ave_line_len}')
+with open(dataset_fn, 'r') as file:
+    dataset = file.readlines()
+
+char_set = set()
+line_count = 0
+ave_line_len = 0
+for line in dataset:
+    line = line.rstrip()
+    if len(line) < 3:
+        continue
+    ave_line_len = (line_count * ave_line_len + len(line)) / (line_count + 1)
+    char_set = char_set.union(set(line))
+    line_count += 1
 
 
-# # ## Prosite
-
-# # In[ ]:
+# In[ ]:
 
 
-# dataset_dir = os.path.join(DATA_ROOT, 'protein/sequences')
-# query_fn = os.path.join(DATA_ROOT, 'protein/prosites.txt')
+print('DBLP')
+print(f'\t\tAlphabet Size: {len(char_set)}')
+print(f'\t\tDataset Size: {line_count}')
+print(f'\t\tAverage Num Chars in Dataset: {ave_line_len}')
 
 
-# # In[ ]:
+# ## Prosite
+
+# In[ ]:
 
 
-# char_set = set()
-# line_count = 0
-# ave_line_len = 0
-# for fn in os.listdir(dataset_dir):
-#     with open(os.path.join(dataset_dir, fn), 'r') as file:
-#         curr_dataset = file.readlines()
-#     for line in curr_dataset:
-#         line = line.rstrip()
-#         ave_line_len = (line_count * ave_line_len + len(line)) / (line_count + 1)
-#         char_set = char_set.union(set(line))
-#         line_count += 1
-
-
-# # In[ ]:
-
-
-# with open(query_fn, 'r') as file:
-#     query_set = file.readlines()
-
-
-# # In[ ]:
-
-
-# print('Protein')
-# print(f'\t\tAlphabet Size: {len(char_set)}')
-# print(f'\t\tDataset Size: {line_count}')
-# print(f'\t\tQuery Set Size: {len(query_set)}')
-# print(f'\t\tAverage Num Chars in Dataset: {ave_line_len}')
-
-
-# # ## US-Accident
-
-# # In[ ]:
-
-
-# dataset_fn = os.path.join(DATA_ROOT, 'US_Accidents_Dec21_updated.csv')
-
-
-# # In[ ]:
-
-
-# with open(dataset_fn, 'r') as file:
-#     dataset = file.readlines()
-
-# char_set = set()
-# line_count = 0
-# ave_line_len = 0
-# for line in dataset:
-#     line = line.rstrip()
-#     ave_line_len = (line_count * ave_line_len + len(line)) / (line_count + 1)
-#     char_set = char_set.union(set(line))
-#     line_count += 1
-
-
-# # In[ ]:
-
-
-# print('US-Accident')
-# print(f'\t\tAlphabet Size: {len(char_set)}')
-# print(f'\t\tDataset Size: {line_count}')
-# print(f'\t\tAverage Num Chars in Dataset: {ave_line_len}')
-
-
-# # ## Sql-Server
-
-# # In[ ]:
-
-
-dataset_dir = os.path.join(DATA_ROOT, 'extracted')
+dataset_dir = os.path.join(DATA_ROOT, 'protein/sequences')
+query_fn = os.path.join(DATA_ROOT, 'protein/prosites.txt')
 
 
 # In[ ]:
@@ -171,6 +101,85 @@ for fn in os.listdir(dataset_dir):
         curr_dataset = file.readlines()
     for line in curr_dataset:
         line = line.rstrip()
+        if len(line) < 3:
+            continue
+        ave_line_len = (line_count * ave_line_len + len(line)) / (line_count + 1)
+        char_set = char_set.union(set(line))
+        line_count += 1
+
+
+# In[ ]:
+
+
+with open(query_fn, 'r') as file:
+    query_set = file.readlines()
+
+
+# In[ ]:
+
+
+print('Protein')
+print(f'\t\tAlphabet Size: {len(char_set)}')
+print(f'\t\tDataset Size: {line_count}')
+print(f'\t\tQuery Set Size: {len(query_set)}')
+print(f'\t\tAverage Num Chars in Dataset: {ave_line_len}')
+
+
+# ## US-Accident
+
+# In[ ]:
+
+
+dataset_fn = os.path.join(DATA_ROOT, 'US_Accidents_Dec21_updated.csv')
+
+
+# In[ ]:
+
+
+with open(dataset_fn, 'r') as file:
+    dataset = file.readlines()
+
+char_set = set()
+line_count = 0
+ave_line_len = 0
+for line in dataset:
+    line = line.rstrip()
+    if len(line) < 3:
+        continue
+    ave_line_len = (line_count * ave_line_len + len(line)) / (line_count + 1)
+    char_set = char_set.union(set(line))
+    line_count += 1
+
+
+# In[ ]:
+
+
+print('US-Accident')
+print(f'\t\tAlphabet Size: {len(char_set)}')
+print(f'\t\tDataset Size: {line_count}')
+print(f'\t\tAverage Num Chars in Dataset: {ave_line_len}')
+
+
+# ## Sql-Server
+
+# In[ ]:
+
+
+dataset_dir = os.path.join(DATA_ROOT, 'extracted')
+
+# In[ ]:
+
+
+char_set = set()
+line_count = 0
+ave_line_len = 0
+for fn in os.listdir(dataset_dir):
+    with open(os.path.join(dataset_dir, fn), 'r') as file:
+        curr_dataset = file.readlines()
+    for line in curr_dataset:
+        line = line.rstrip()
+        if len(line) < 3:
+            continue
         ave_line_len = (line_count * ave_line_len + len(line)) / (line_count + 1)
         char_set = char_set.union(set(line))
         line_count += 1
@@ -179,3 +188,18 @@ print('SQL-Server')
 print(f'\t\tAlphabet Size: {len(char_set)}')
 print(f'\t\tDataset Size: {line_count}')
 print(f'\t\tAverage Num Chars in Dataset: {ave_line_len}')
+
+query_fn = os.path.join(DATA_ROOT, 'db-x_mid.txt')
+with open(query_fn, 'r') as file:
+    query_set = file.readlines()
+print(f'\t\tQuery Set Mid Size: {len(query_set)}')
+
+query_fn = os.path.join(DATA_ROOT, 'db-x_full.txt')
+with open(query_fn, 'r') as file:
+    query_set = file.readlines()
+print(f'\t\tQuery Set Full Size: {len(query_set)}')
+
+query_fn = os.path.join(DATA_ROOT, 'regexes_dbx.txt')
+with open(query_fn, 'r') as file:
+    query_set = file.readlines()
+print(f'\t\tQuery Set Default Size: {len(query_set)}')
