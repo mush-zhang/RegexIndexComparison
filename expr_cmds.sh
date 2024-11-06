e767c96bc63530f8d01252b7a6b1b924078634a9
LING ZHANG <junelena1313@gmail.com>
2024-11-06 16:14:34 -0600

    Update expr_cmds.sh

--- expr_cmds.sh
+++ expr_cmds.sh
@@ -16,7 +16,7 @@ done
 
 # dblp
 for qcount in 1000 2000 3000 4000; do
-    ./run_expr.sh -w dblp_${qcount} \
+    ./run_expr.sh -w dblp_small_${qcount} \
     -r data/dblp/small/query${qcount}.txt \
     -d data/dblp/small/authors.txt
 done
