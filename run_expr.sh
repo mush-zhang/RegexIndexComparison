72f4547ceebac2deefc24c878951d81bdeed371e
LING ZHANG <junelena1313@gmail.com>
2024-11-11 16:02:00 -0600

    Update run_expr.sh

--- run_expr.sh
+++ run_expr.sh
@@ -40,12 +40,13 @@ if [ "$wl_num" == "0" ]; then
     extra="-r ${regex_file} -d ${data_file}"
 fi
 
-timeout_prefix="{timeout 1h time -v "
+timeout_prefix="{timeout 3h time -v "
 timeout_suffix=";} 2> ${dirname}/time_report"
 
 echo ${dirname}
 mkdir -p ${dirname} 
-sel_list=( 0.01 0.02 0.03 0.05 0.07 0.1 0.12 0.15 0.2 0.3 0.5 0.7)
+# sel_list=( 0.01 0.02 0.03 0.05 0.07 0.1 0.12 0.15 0.2 0.3 0.5 0.7)
+sel_list=( 0.05 0.1 0.12 0.15 0.2 0.5 0.7)
 num_repeat=1
 
 # Best
@@ -59,7 +60,8 @@ if [ ${wl_num} != 1 ]; then
     done
 fi
 # Free
-for n in 2 4 6 8 10 12 14 16; do
+# for n in 2 4 6 8 10 12 14 16; do
+for n in 2 4 6; do
     for c in ${sel_list[*]}; do
         # echo benchmark.out FREE -t 1 -w ${wl_num} -o ${dirname} -n ${n} --presuf -c ${c} -e ${num_repeat} ${extra}
         # ${timeout_prefix} ./benchmark.out FREE -t 1 -w ${wl_num} -o ${dirname} -n ${n} --presuf -c ${c} -e ${num_repeat} ${extra}
@@ -70,6 +72,7 @@ for n in 2 4 6 8 10 12 14 16; do
         done
     done
 done
+
 # Fast
 for t in ${thread_list[*]}; do
     curr_suffix="${timeout_suffix}_lpms_t${t}_determ.txt"
