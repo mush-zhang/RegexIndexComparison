0ece8a7dc7f6ab396f1be4c10e644de62df4c456
LING ZHANG <junelena1313@gmail.com>
2025-02-16 15:19:09 -0600

    temp comment out

--- temp_lpms_expr.sh
+++ temp_lpms_expr.sh
@@ -1,36 +1,36 @@
 #! /bin/bash
 
-dblp
-kups=( 20 50 100 150 200 500 1000 2000 )
-for k in ${kups[*]}; do
-    for qcount in 1000 2000; do
-        ./run_expr_lpms.sh -w dblp_small_${qcount} \
-        -r data/dblp/small/query${qcount}.txt \
-        -d data/dblp/small/authors.txt -k ${k}
-    done
-done
+# dblp
+# kups=( 20 50 100 150 200 500 1000 2000 )
+# for k in ${kups[*]}; do
+#     for qcount in 1000 2000; do
+#         ./run_expr_lpms.sh -w dblp_small_${qcount} \
+#         -r data/dblp/small/query${qcount}.txt \
+#         -d data/dblp/small/authors.txt -k ${k}
+#     done
+# done
 
-# webpage
-kups=( 5 10 15 100 )
-for k in ${kups[*]}; do 
-    ./run_expr_lpms.sh -w webpages -k ${k}
-done
+# # webpage
+# kups=( 5 10 15 100 )
+# for k in ${kups[*]}; do 
+#     ./run_expr_lpms.sh -w webpages -k ${k}
+# done
 
-# Protein
-kups=( 50 100 200 500 1000 )
-for k in ${kups[*]}; do
-    ./run_expr_lpms.sh -w protein -k ${k}
-done
+# # Protein
+# kups=( 50 100 200 500 1000 )
+# for k in ${kups[*]}; do
+#     ./run_expr_lpms.sh -w protein -k ${k}
+# done
 
-# DB_X
-for k in ${kups[*]}; do
-    ./run_expr_lpms.sh -w db_x -k ${k}
-done
+# # DB_X
+# for k in ${kups[*]}; do
+#     ./run_expr_lpms.sh -w db_x -k ${k}
+# done
 
-# traffic
-for k in 5 10 15; do
-    ./run_expr_lpms.sh -w traffic  -k ${k}
-done
+# # traffic
+# for k in 5 10 15; do
+#     ./run_expr_lpms.sh -w traffic  -k ${k}
+# done
 
 # robust
 for rob_wl in 1 2 3 4; do
