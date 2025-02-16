d94b972226921401cfae90f2ec8654c4ec1da416
LING ZHANG <junelena1313@gmail.com>
2025-02-16 15:28:12 -0600

    fix parameter -q

--- temp_free_expr.sh
+++ temp_free_expr.sh
@@ -37,7 +37,7 @@ for rob_wl in 1 2 3 4; do
     for perc in 10 30 50; do
         ./run_expr_free.sh -w synthetic_expr4_rob0${rob_wl}_${perc} \
         -r data/synthetic/expr4/queries/Rob0${rob_wl}_queries_${perc}pct.txt \
-        -r data/synthetic/expr4/queries/Rob0${rob_wl}_test_queries_2pct.txt \
+        -q data/synthetic/expr4/queries/Rob0${rob_wl}_test_queries_2pct.txt \
         -d data/synthetic/expr4/datasets/Rob0${rob_wl}.txt
     done
 done
