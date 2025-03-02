170151255291b08c54fbc54152628d4df8f69584
LING ZHANG <junelena1313@gmail.com>
2025-03-01 18:21:53 -0600

    Delete dblp.sh

--- dblp.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
- 
-# dblp
-for qcount in 1000 2000 3000 4000; do
-    ./run_expr.sh -w dblp_small_${qcount} \
-    -r data/dblp/small/query${qcount}.txt \
-    -d data/dblp/small/authors.txt
-done
-
