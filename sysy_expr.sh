5c66e9dcbd6b5ee4173d6051235ef01086646529
LING ZHANG <junelena1313@gmail.com>
2025-05-05 14:15:25 +0800

    Update sysy_expr.sh

--- sysy_expr.sh
+++ sysy_expr.sh
@@ -4,7 +4,7 @@
 sysydir=result/sysy_results
 mkdir ${sysydir}
 
-kups = ( 50 100 500 1000 2000 5000 10000 )
+kups=( 50 100 500 1000 2000 5000 10000 )
 for k in ${kups[*]}; do
         { time -v ./benchmark.out FREE -t 16 -w 5 -o ${sysydir} -n 2 -c 0.7 -e 10 -k ${k} ; } 2> ${sysydir}/time_report_free_t16_c0.7_n2_${k}.txt
         echo "end executing FREE max_key=${k}"
