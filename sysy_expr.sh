cd51bd3d1fbb917f28f6a274be5418f89a02c0e0
LING ZHANG <junelena1313@gmail.com>
2025-05-07 15:04:21 +0800

    new sys y expr

--- sysy_expr.sh
+++ sysy_expr.sh
@@ -4,20 +4,40 @@
 sysydir=result/sysy_results
 mkdir ${sysydir}
 
-kups=( 50 100 500 1000 2000 5000 10000 )
-for k in ${kups[*]}; do
+# kups=( 50 100 500 1000 2000 5000 10000 )
+# for k in ${kups[*]}; do
+#         /usr/bin/time -v ./benchmark.out FREE -t 16 -w 5 -o ${sysydir} -n 2 -c 0.7 -e 10 -k ${k}  2> ${sysydir}/time_report_free_t16_c0.7_n2_${k}.txt
+#         echo "end executing FREE max_key=${k}"
+# done
+
+# for k in 5 10 20; do
+#         /usr/bin/time -v ./benchmark.out LPMS -t 16 -w 5 -o ${sysydir} --relax DETERM -e 10 -k ${k}  2> ${sysydir}/time_report_lpms_t16_determ_${k}.txt
+#         echo "end executing LPMS max_key=${k}"
+# done
+
+# for k in 5 10 20; do
+#         /usr/bin/time -v ./benchmark.out BEST -t 16 -w 5 -o ${sysydir} -c 0.1 -e 10 -k ${k}  2> ${sysydir}/time_report_best_t16_c0.1_${k}.txt
+#         echo "end executing BEST c=0.1 max_key=${k}"
+#         /usr/bin/time -v ./benchmark.out BEST -t 16 -w 5 -o ${sysydir} -c 0.5 -e 10 -k ${k}  2> ${sysydir}/time_report_best_t16_c0.5_${k}.txt
+#         echo "end executing BEST c=0.5 max_key=${k}"
+# done
+
+for k in 5 10 20; do
         /usr/bin/time -v ./benchmark.out FREE -t 16 -w 5 -o ${sysydir} -n 2 -c 0.7 -e 10 -k ${k}  2> ${sysydir}/time_report_free_t16_c0.7_n2_${k}.txt
         echo "end executing FREE max_key=${k}"
 done
 
-for k in 5 10 20; do
+for k in 5 10 20 50; do
         /usr/bin/time -v ./benchmark.out LPMS -t 16 -w 5 -o ${sysydir} --relax DETERM -e 10 -k ${k}  2> ${sysydir}/time_report_lpms_t16_determ_${k}.txt
         echo "end executing LPMS max_key=${k}"
 done
 
-for k in 5 10 20; do
+for k in 20 50; do
         /usr/bin/time -v ./benchmark.out BEST -t 16 -w 5 -o ${sysydir} -c 0.1 -e 10 -k ${k}  2> ${sysydir}/time_report_best_t16_c0.1_${k}.txt
         echo "end executing BEST c=0.1 max_key=${k}"
+done
+
+for k in 5 10 20 50; do
         /usr/bin/time -v ./benchmark.out BEST -t 16 -w 5 -o ${sysydir} -c 0.5 -e 10 -k ${k}  2> ${sysydir}/time_report_best_t16_c0.5_${k}.txt
         echo "end executing BEST c=0.5 max_key=${k}"
 done
