25daf6ee38882dbef6975946ba583817516eea0e
lzhang55 <lzhang55@ebay.com>
2025-06-30 10:48:42 -0700

    Update rerun_synthetic.sh

--- rerun_synthetic.sh
+++ rerun_synthetic.sh
@@ -2,7 +2,7 @@
 
 synthetic_dir=result/synthetic2_50000_qs500
 mkdir -p ${synthetic_dir}
-for selectivity in 0.001 0.005 0.010 0.02 0.05 0.1 0.2 0.5; do
+for selectivity in '0.001' '0.005' '0.010' '0.020' '0.050' '0.100' '0.200' '0.500'; do
     ./run_expr_baseline.sh -d data/configurable_synthetic/dataset_alph16_data${datasetSize}_qs500_sel${selectivity}.txt \
     -r data/configurable_synthetic/queries_alph16_data${datasetSize}_qs500_sel${selectivity}.txt \
     -w synthetic_${datasetSize}
